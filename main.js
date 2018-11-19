6fe1ea115ec54e0281a491c59716d6708cbbfc07
長谷川 泰斗 <x18n070@chiba-fjb.ac.jp>
2018-11-19 14:40:51 +0900

    bug fixed i will add jquery

--- main.js
+++ main.js
@@ -1,10 +1,9 @@
-var mySwiper = new Swiper('.swiper-container', {
+var mySwiper = new Swiper ('.swiper-container', {
   effect: "flip",
   loop: true,
   pagination: '.swiper-pagination',
   nextButton: '.swiper-button-next',
   prevButton: '.swiper-button-prev',
 })
-
 	
 
