b08bde3209567234811def6ac9e53ce91e18c659
長谷川 泰斗 <x18n070@chiba-fjb.ac.jp>
2018-11-19 15:13:16 +0900

    jquery added

--- main.js
+++ main.js
@@ -6,4 +6,24 @@ var mySwiper = new Swiper ('.swiper-container', {
   prevButton: '.swiper-button-prev',
 })
 	
-
+// top ボタン
+	
+$(function () {
+    var topBtn = $('#page-top');
+    topBtn.hide();
+    //スクロールが500に達したらボタン表示
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 500) {
+            topBtn.fadeIn();
+        } else {
+            topBtn.fadeOut();
+        }
+    });
+    //スルスルっとスクロールでトップへもどる
+    topBtn.click(function () {
+        $('body,html').animate({
+            scrollTop: 0
+        }, 500);
+        return false;
+    });
+});
