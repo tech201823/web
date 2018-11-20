152daf1d3bd055b5302c5d320976370f93461b83
長谷川 泰斗 <x18n070@chiba-fjb.ac.jp>
2018-11-20 15:17:07 +0900

    added animation and i will add  site header

--- js/main.js
+++ js/main.js
@@ -41,3 +41,11 @@ $(window).scroll(function () {
     });
 });
 
+// para
+var particles = Particles.init({
+    selector: '.background',
+  sizeVariations: 10,
+  color: ['#00bbdd', '#404B69', '#DBEDF3'],
+  connectParticles: true
+});
+
