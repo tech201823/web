ced203f8e31b558c1b3d2a69c6d67aa2efaf10b7
長谷川 泰斗 <x18n070@chiba-fjb.ac.jp>
2018-11-22 15:16:06 +0900

    swiper auto play

--- js/main.js
+++ js/main.js
@@ -1,11 +1,16 @@
-var mySwiper = new Swiper('.swiper-container', {
-    effect: "flip",
-    loop: true,
-    pagination: '.swiper-pagination',
-    nextButton: '.swiper-button-next',
-    prevButton: '.swiper-button-prev',
-})
 
+// swiper auto play
+var swiper = new Swiper('.swiper-container', {
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    loop: true,
+    autoplay: {
+      delay:4000,
+      disableOnInteraction: false,
+    },
+  });
 // top ボタン
 
 $(function () {
