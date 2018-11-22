819f6a637d0ee36ee7d8bac2feb157873f03e1a8
tech201823 <tech201823@gmail.com>
2018-11-22 23:23:57 +0900

    フォーマット完了

--- js/main.js
+++ js/main.js
@@ -1,200 +1,191 @@
-
 // swiper auto play
-var swiper = new Swiper('.swiper-container', {
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-    loop: true,
-    autoplay: 4000,
-    delay: 4000,
-    disableOnInteraction: true,
-    effect: 'fade',
-    pagination: '.swiper-pagination',
-
+var swiper = new Swiper(".swiper-container", {
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev"
+  },
+  loop: true,
+  autoplay: 4000,
+  delay: 4000,
+  disableOnInteraction: true,
+  effect: "fade",
+  pagination: ".swiper-pagination"
 });
 // top ボタン
 
-$(function () {
-    var topBtn = $('#page-top');
-    topBtn.hide();
-    //スクロールが500に達したらボタン表示
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 870) {
-            topBtn.fadeIn();
-        } else {
-            topBtn.fadeOut();
-        }
-    });
-    //スルスルっとスクロールでトップへもどる
-    topBtn.click(function () {
-        $('body,html').animate({
-            scrollTop: 0
-        }, 700);
-        return false;
-    });
+$(function() {
+  var topBtn = $("#page-top");
+  topBtn.hide();
+  //スクロールが500に達したらボタン表示
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 870) {
+      topBtn.fadeIn();
+    } else {
+      topBtn.fadeOut();
+    }
+  });
+  //スルスルっとスクロールでトップへもどる
+  topBtn.click(function() {
+    $("body,html").animate(
+      {
+        scrollTop: 0
+      },
+      700
+    );
+    return false;
+  });
 });
 
 // 文字浮き上がる
-$('#animation').css('visibility', 'hidden');
-$(window).scroll(function () {
-    var windowHeight = $(window).height(),
-        topWindow = $(window).scrollTop();
-    $('#animation').each(function () {
-        var targetPosition = $(this).offset().top;
-        if (topWindow > targetPosition - windowHeight + 100) {
-            $(this).addClass("fadeInDown");
-        }
-    });
+$("#animation").css("visibility", "hidden");
+$(window).scroll(function() {
+  var windowHeight = $(window).height(),
+    topWindow = $(window).scrollTop();
+  $("#animation").each(function() {
+    var targetPosition = $(this).offset().top;
+    if (topWindow > targetPosition - windowHeight + 100) {
+      $(this).addClass("fadeInDown");
+    }
+  });
 });
 
 // para
 var particles = Particles.init({
-    selector: '.background',
-    sizeVariations: 10,
-    color: ['#00bbdd', '#404B69', '#DBEDF3'],
-    connectParticles: true
+  selector: ".background",
+  sizeVariations: 10,
+  color: ["#00bbdd", "#404B69", "#DBEDF3"],
+  connectParticles: true
 });
 
 // textillate
 // added 個別設定
 
+$(".tlt1").textillate({
+  // the default selector to use when detecting multiple texts to animate
+  selector: ".texts",
 
-$('.tlt1').textillate({
-    // the default selector to use when detecting multiple texts to animate
-    selector: '.texts',
+  // enable looping
+  loop: true,
 
-    // enable looping
-    loop: true,
+  // sets the minimum display time for each text before it is replaced
+  minDisplayTime: 2000,
 
-    // sets the minimum display time for each text before it is replaced
-    minDisplayTime: 2000,
+  // sets the initial delay before starting the animation
+  // (note that depending on the in effect you may need to manually apply
+  // visibility: hidden to the element before running this plugin)
+  initialDelay: 0,
 
-    // sets the initial delay before starting the animation
-    // (note that depending on the in effect you may need to manually apply
-    // visibility: hidden to the element before running this plugin)
-    initialDelay: 0,
+  // set whether or not to automatically start animating
+  autoStart: true,
 
-    // set whether or not to automatically start animating
-    autoStart: true,
+  // アニメーションの間隔時間
+  minDisplayTime: 3000,
 
-    // アニメーションの間隔時間
-    minDisplayTime: 3000,
+  // in animation settings
+  in: {
+    // set the effect name
+    effect: "fadeInDownBig",
 
+    // set the delay factor applied to each consecutive character
+    delayScale: 1.5,
 
+    // set the delay between each character
+    delay: 50,
 
-    // in animation settings
-    in: {
+    // set to true to animate all the characters at the same time
+    sync: false,
 
-        // set the effect name
-        effect: 'fadeInDownBig',
+    // randomize the character sequence
+    // (note that shuffle doesn't make sense with sync = true)
+    shuffle: true,
 
-        // set the delay factor applied to each consecutive character
-        delayScale: 1.5,
+    // reverse the character sequence
+    // (note that reverse doesn't make sense with sync = true)
+    reverse: true,
 
-        // set the delay between each character
-        delay: 50,
+    // callback that executes once the animation has finished
+    callback: function() {}
+  },
 
-        // set to true to animate all the characters at the same time
-        sync: false,
+  // out animation settings.
+  out: {
+    effect: "rollOut",
+    delayScale: 1.5,
+    delay: 50,
+    sync: false,
+    shuffle: true,
+    reverse: true,
+    callback: function() {}
+  },
 
-        // randomize the character sequence
-        // (note that shuffle doesn't make sense with sync = true)
-        shuffle: true,
+  // callback that executes once textillate has finished
+  callback: function() {},
 
-        // reverse the character sequence
-        // (note that reverse doesn't make sense with sync = true)
-        reverse: true,
-
-        // callback that executes once the animation has finished
-        callback: function () { }
-    },
-
-    // out animation settings.
-    out: {
-        effect: 'rollOut',
-        delayScale: 1.5,
-        delay: 50,
-        sync: false,
-        shuffle: true,
-        reverse: true,
-        callback: function () { }
-    },
-
-    // callback that executes once textillate has finished
-    callback: function () { },
-
-    // set the type of token to animate (available types: 'char' and 'word')
-    type: 'char'
+  // set the type of token to animate (available types: 'char' and 'word')
+  type: "char"
 });
 
+$(".tlt2").textillate({
+  // the default selector to use when detecting multiple texts to animate
+  selector: ".texts",
 
-$('.tlt2').textillate({
-    // the default selector to use when detecting multiple texts to animate
-    selector: '.texts',
+  // enable looping
+  loop: true,
 
-    // enable looping
-    loop: true,
+  // sets the minimum display time for each text before it is replaced
+  minDisplayTime: 2000,
 
-    // sets the minimum display time for each text before it is replaced
-    minDisplayTime: 2000,
+  // sets the initial delay before starting the animation
+  // (note that depending on the in effect you may need to manually apply
+  // visibility: hidden to the element before running this plugin)
+  initialDelay: 0,
 
-    // sets the initial delay before starting the animation
-    // (note that depending on the in effect you may need to manually apply
-    // visibility: hidden to the element before running this plugin)
-    initialDelay: 0,
+  // set whether or not to automatically start animating
+  autoStart: true,
 
-    // set whether or not to automatically start animating
-    autoStart: true,
+  // アニメーションの間隔時間
+  minDisplayTime: 2000,
 
-    // アニメーションの間隔時間
-    minDisplayTime: 2000,
+  // in animation settings
+  in: {
+    // set the effect name
+    effect: "fadeInLeftBig",
 
+    // set the delay factor applied to each consecutive character
+    delayScale: 1.5,
 
+    // set the delay between each character
+    delay: 50,
 
-    // in animation settings
-    in: {
-        // set the effect name
-        effect: 'fadeInLeftBig',
+    // set to true to animate all the characters at the same time
+    sync: false,
 
-        // set the delay factor applied to each consecutive character
-        delayScale: 1.5,
+    // randomize the character sequence
+    // (note that shuffle doesn't make sense with sync = true)
+    shuffle: true,
 
-        // set the delay between each character
-        delay: 50,
+    // reverse the character sequence
+    // (note that reverse doesn't make sense with sync = true)
+    reverse: true,
 
-        // set to true to animate all the characters at the same time
-        sync: false,
+    // callback that executes once the animation has finished
+    callback: function() {}
+  },
 
-        // randomize the character sequence
-        // (note that shuffle doesn't make sense with sync = true)
-        shuffle: true,
+  // out animation settings.
+  out: {
+    effect: "rollOut",
+    delayScale: 1.5,
+    delay: 100,
+    sync: false,
+    shuffle: true,
+    reverse: false,
+    callback: function() {}
+  },
 
-        // reverse the character sequence
-        // (note that reverse doesn't make sense with sync = true)
-        reverse: true,
+  // callback that executes once textillate has finished
+  callback: function() {},
 
-        // callback that executes once the animation has finished
-        callback: function () { }
-    },
-
-    // out animation settings.
-    out: {
-        effect: 'rollOut',
-        delayScale: 1.5,
-        delay: 100,
-        sync: false,
-        shuffle: true,
-        reverse: false,
-        callback: function () { }
-    },
-
-    // callback that executes once textillate has finished
-    callback: function () { },
-
-    // set the type of token to animate (available types: 'char' and 'word')
-    type: 'char'
+  // set the type of token to animate (available types: 'char' and 'word')
+  type: "char"
 });
-
-
-
